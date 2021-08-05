4355234173d7badd100e81f6d9154457c8fa480c
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-08-05 00:06:52 -0500

    Add project files.

---
+++ PlanetsAndSpaceships/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            var lastPlanets = new List<string>() { "Uranus", "Neptune" };
+            planetList.AddRange(lastPlanets);
+            planetList.Insert(1, "Earth");
+            planetList.Insert(1, "Venus");
+            planetList.Add("Pluto");
+            var rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+            Console.WriteLine($"planets: {String.Join(",", planetList)}");
+            Console.WriteLine($"rocky planets: {String.Join(",", rockyPlanets)}");
+
+        }
+    }
+}
